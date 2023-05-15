113a7b55058e57dae6d86f9f671fe8b16dcc6cd2
rudenkoserhii <103450718+rudenkoserhii@users.noreply.github.com>
2023-05-15 12:55:48 +0000

    Deploying to gh-pages from @ rudenkoserhii/cv@2c65993294282180a171de6277c4f3577e3018e2 🚀

---
+++ static/js/812.ffe216ba.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcv=self.webpackChunkcv||[]).push([[812],{1812:function(e,r,s){s.r(r),s.d(r,{default:function(){return l}});var t,n=s(168),c=s(6444).ZP.img(t||(t=(0,n.Z)(["\n"]))),u=s(2791),a=s(3260),i=s(184),l=function(){var e=(0,u.useContext)(a.r).selected;return e&&(0,i.jsxs)("picture",{children:[(0,i.jsx)("source",{srcSet:e.urlWebp,type:"image/webp"}),(0,i.jsx)("source",{srcSet:e.urlPng,type:"image/png"}),(0,i.jsx)(c,{src:e.urlPng,alt:"Man",width:"800px",className:"first"})]},e.name)}}}]);
+//# sourceMappingURL=812.ffe216ba.chunk.js.map
